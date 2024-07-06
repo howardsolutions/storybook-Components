6afcc5e5c6bf0f37a81e4712f486305f6420606e
thinhphungdev <phungthinh0946519356@gmail.com>
2024-07-06 17:54:27 +0700

    style : add dark mode theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,4 +3,5 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.mdx'],
   plugins: [],
+  darkMode: ['class', '[data-mode="dark"]'],
 } satisfies Config;
