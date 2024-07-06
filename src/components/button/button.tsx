4098fd6ddc5529d76f273731b0b522bf9f3e4557
thinhphungdev <phungthinh0946519356@gmail.com>
2024-07-06 15:48:04 +0700

    style: css btn size & adjust btn props type

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,10 +4,11 @@ import clsx from 'clsx';
 
 type ButtonProps = ComponentProps<'button'> & {
   variant?: 'primary' | 'secondary' | 'destructive';
+  size?: 'small' | 'medium' | 'large';
 };
 
-export const Button = ({ variant = 'primary', ...props }: ButtonProps) => {
-  const buttonStyles = clsx(styles.button, variant && styles[variant]);
+export const Button = ({ variant = 'primary', size = 'small', ...props }: ButtonProps) => {
+  const buttonStyles = clsx(styles.button, variant && styles[variant], size && styles[size]);
 
   return <button className={buttonStyles} {...props} />;
 };
